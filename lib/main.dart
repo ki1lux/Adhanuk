53ebad26910e0ae591b24f2884571a37f1eaf120
khalil benfiala <bmama5200@gmail.com>
2025-05-24 20:38:40 +0100

    create navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // import 'dart:nativewrappers/_internal/vm/lib/ffi_patch.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 // import 'package:flutter_svg/svg.dart';
 // import 'package:myadhan/view/AnalogClockView.dart';
 // import 'package:geolocator/geolocator.dart';
@@ -20,6 +21,14 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int _slectIndex = 1;
+
+  void _onTap(int index) {
+    setState(() {
+      _slectIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,11 +36,47 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         extendBody: true,
         body: AdhanScreen(),
-      bottomNavigationBar: BottomNavigationBar(items: [
-            BottomNavigationBarItem (icon: Icon(Icons.home), label: "Home"),
-            BottomNavigationBarItem(icon: Icon(Icons.compass_calibration), label: "Compass"),
-            BottomNavigationBarItem(icon: Icon(Icons.access_time), label: "Time"),
-          ]),
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.only(bottom: 16, right: 12, left: 12),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Container(
+              height: 58,
+              // padding: EdgeInsets.symmetric(vertical: 16),
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.05),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  _buildNavItem('assets/h1.svg', 0),
+                  _buildNavItem('assets/h2.svg', 1),
+                  _buildNavItem('assets/h3.svg', 2),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNavItem(String assets, int index) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: () => _onTap(index),
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: SvgPicture.asset(
+            assets,
+            colorFilter: ColorFilter.mode(
+              _slectIndex == index ? Colors.white : Colors.white60,
+              BlendMode.srcIn,
+            ),
+          ),
+        ),
       ),
     );
   }
