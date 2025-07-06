8bafff3d83d2f15c8d92a4a5e1af7a0b708e31de
khalil benfiala <ahmed.benfiala@etu.univ-batna2.dz>
2025-07-06 19:54:18 +0100

    fix quality icons and compass

--- lib/view/QiblaScreen.dart
+++ lib/view/QiblaScreen.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_qiblah/flutter_qiblah.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:myadhan/controller/QiblahController.dart';
 import 'package:myadhan/view/CompassView.dart';
 
@@ -38,29 +39,36 @@ class _QiblaScreenState extends State<QiblaScreen> {
               return Center(
                 child: Stack(
                   children: [
+                    SvgPicture.asset(
+                      'assets/Vector.svg',
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                      height: double.infinity,
+                    ),
+
                     Positioned(
-                      top: -70,
-                      bottom: -70,
-                      left: -70,
-                      right: -70,
+                      top: -100,
+                      bottom: -100,
+                      left: -100,
+                      right: -100,
                       child: Transform.rotate(
                         angle:
                             ((qiblahDirection.direction ?? 0) *
                                 (pi / 180) *
                                 -1),
-                        child: Image.asset("assets/normal_compass.png"),
+                        child: SvgPicture.asset("assets/normal_compass.svg"),
                       ),
                     ),
 
                     Positioned(
-                      top: 100,
-                      bottom: 100,
-                      left: 100,
-                      right: 100,
+                      top: -130,
+                      bottom: -130,
+                      left: -130,
+                      right: -130,
                       child: Transform.rotate(
                         angle:
                             ((qiblahDirection.qiblah ?? 0) * (pi / 180) * -1),
-                        child: Image.asset("assets/qiblahanim1.png"),
+                        child: SvgPicture.asset("assets/qiblah_direction.svg"),
                       ),
                     ),
                     Center(
