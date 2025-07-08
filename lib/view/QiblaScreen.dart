cb506ecca402422c7c4f7221643bcf659ea02977
khalil benfiala <ahmed.benfiala@etu.univ-batna2.dz>
2025-07-08 20:34:38 +0100

    update ui of compass and fix some bugs

--- lib/view/QiblaScreen.dart
+++ lib/view/QiblaScreen.dart
@@ -1,10 +1,7 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_qiblah/flutter_qiblah.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:myadhan/controller/QiblahController.dart';
-
 
 class QiblaScreen extends StatefulWidget {
   @override
@@ -13,77 +10,77 @@ class QiblaScreen extends StatefulWidget {
 
 class _QiblaScreenState extends State<QiblaScreen> {
   final QiblahController _controller = QiblahController();
-  bool _isPermissionGranted = false;
+
   @override
   void initState() {
+    // TODO: implement initState
     super.initState();
-    checkPermissions();
-  }
-
-  Future<void> checkPermissions() async {
-    await FlutterQiblah.requestPermissions();
-    setState(() {
-      _isPermissionGranted = true;
-    });
+    _controller.init();
   }
 
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return Scaffold(
       backgroundColor: const Color(0xff0A2239),
       body: SafeArea(
-        child: _isPermissionGranted
-            ? StreamBuilder<QiblahDirection>(
-                stream: FlutterQiblah.qiblahStream,
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return Center(child: CircularProgressIndicator());
-                  }
+        child: Center(
+          child: StreamBuilder(
+            stream: _controller.getQiblaStream(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return Center(child: CircularProgressIndicator());
+              }
+              final qiblahDirection = snapshot.data;
+              final double screenWidth = MediaQuery.of(context).size.width;
+              return SizedBox(
+                height: screenWidth + 70,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    SvgPicture.asset(
+                      'assets/Vector.svg',
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                      height: double.infinity,
+                    ),
 
-                  final qiblahDirection = snapshot.data!;
+                    Transform.rotate(
+                      angle:
+                          ((qiblahDirection.direction ?? 0) * (pi / 180) * -1),
+                      child: SvgPicture.asset("assets/test.svg"),
+                    ),
 
-                  return Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      SvgPicture.asset(
-                        'assets/Vector.svg',
-                        fit: BoxFit.cover,
-                        width: double.infinity,
-                        height: double.infinity,
-                      ),
+                    Transform.rotate(
+                      angle: ((qiblahDirection.qiblah ?? 0) * (pi / 180) * -1),
+                      child: SvgPicture.asset("assets/ka3baInCompass.svg"),
+                    ),
 
-                      Transform.rotate(
-                        angle: (qiblahDirection.direction * (pi / 180) * -1),
-                        child: SvgPicture.asset("assets/normal_compass.svg"),
-                      ),
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: SvgPicture.asset("assets/arrow.svg"),
+                    ),
 
-                      Transform.rotate(
-                        angle: (qiblahDirection.qiblah * (pi / 180) * -1),
-                        child: SvgPicture.asset("assets/qiblah_direction.svg"),
-                      ),
-
-                      Positioned(
-                        bottom: 32,
-                        child: Text(
-                          "${qiblahDirection.offset.toStringAsFixed(0)}°",
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontFamily: 'cairo',
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
+                    Center(
+                      child: Text(
+                        " ${qiblahDirection.offset.toStringAsFixed(0)}°",
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontFamily: 'cairo',
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                    ],
-                  );
-                },
-              )
-            : Center(child: CircularProgressIndicator()),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
       ),
     );
   }
 }
-
 
 // Widget _buildDirectionLabel(String label, double angle, double radius) {
 //   return Transform.translate(
